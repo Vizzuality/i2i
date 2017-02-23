b1874d4ec221d7fc9a1b415587debc6346e72392
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-02-23 17:21:25 +0100

    Toggle content library

--- app/assets/javascripts/routers/application.js
+++ app/assets/javascripts/routers/application.js
@@ -2,11 +2,16 @@ App.Router.StaticPages = Backbone.Router.extend({
 
   routes: {
     '': '_homePage',
+    libraries: '_libraryPage',
     about: '_aboutPage'
   },
 
   _homePage: function () {
     new App.Page.HomePage();
+  },
+
+  _libraryPage: function () {
+    new App.Page.LibraryPage();
   },
 
   _aboutPage: function () {
