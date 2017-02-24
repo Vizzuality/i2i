2d9f423cb701a1e672b0a73164c212e3c643ac26
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-02-24 15:49:11 +0000

    Adds loading & error states of the data portal detail page

--- app/assets/javascripts/pages/data_portal/DataPortalCountryPage.js
+++ app/assets/javascripts/pages/data_portal/DataPortalCountryPage.js
@@ -29,9 +29,24 @@
     { indicator: 'i2i_Income_Sources' }
   ];
 
+  // This will be removed when we have a way to get the country name from the API
+  var COUNTRIES = {
+    UGA: 'Uganda',
+    TZA: 'Tanzania',
+    ZMB: 'Zambia',
+    RWA: 'Rwanda',
+    GHA: 'Ghana',
+    KEN: 'Kenya',
+    MOZ: 'Mozambique',
+    PAK: 'Pakistan'
+  };
+
   App.Page.DataPortalCountryPage = Backbone.View.extend({
 
-    widgetsTemplate: JST['templates/data_portal/country-page'],
+    el: 'body',
+
+    headerTemplate: JST['templates/data_portal/header'],
+    widgetsTemplate: JST['templates/data_portal/widgets'],
 
     defaults: {
       // ISO of the country
@@ -40,26 +55,38 @@
       year: null
     },
 
+    events: {
+      'click .js-retry': '_fetchData'
+    },
+
     initialize: function (settings) {
       this.options = _.extend({}, this.defaults, settings);
       // this.indicatorsCollection = new IndicatorsCollection(this.options.iso, this.options.year);
       this.indicatorsCollection = new IndicatorsCollection(INDICATORS);
-      this.widgetsContainer = document.querySelector('.js-widgets');
-      this.fetchData()
-        .done(this.render.bind(this))
-        .fail(this.renderError.bind(this));
+      this.headerContainer = this.el.querySelector('.js-header');
+      this.widgetsContainer = this.el.querySelector('.js-widgets');
+      this._fetchData();
     },
 
     /**
      * Fetch the model for the selected country and year
-     * @returns {object} $.Deferred
      */
-    fetchData: function () {
+    _fetchData: function () {
+      // The first time we render to have the placeholder flags
+      this.render();
+
       var deferred = $.Deferred();
       deferred.resolve(INDICATORS);
-      return deferred;
 
-      // return this.indicatorsCollection.fetch();
+      // this.indicatorsCollection.fetch()
+      deferred
+        .done(function (){
+          this._loadingError = false;
+        }.bind(this))
+        .fail(function () {
+          this._loadingError = true;
+        }.bind(this))
+        .always(this.render.bind(this));
     },
 
     render: function () {
@@ -72,30 +99,67 @@
       // // We replace the placeholder by the year
       // document.querySelector('.js-year').textContent = this.options.year;
 
-      this.renderWidgets();
-    },
+      this._renderHeader();
+      this._renderWidgets();
 
-    renderError: function () {
-      // TODO
-      console.error('unable to fetch the country data');
+      this.setElement(this.el);
     },
 
     /**
-     * Render the widgets
+     * Render the header
      */
-    renderWidgets: function () {
-      var collection = this.indicatorsCollection.toJSON();
+    _renderHeader: function () {
+      this.headerContainer.innerHTML = this.headerTemplate({
+        error: this._loadingError,
+        indicators: this.indicatorsCollection.toJSON(),
+        year: this.options.year,
+        country: COUNTRIES[this.options.iso]
+      });
+    },
+
+    /**
+     * Create the widget containers
+     * @param {number} count - number of containers to create
+     * @returns {DocumentFragment} containers
+     */
+    _createWidgetsContainer: function (count) {
       var fragment = new DocumentFragment();
 
-      // We create the containers for the widgets
-      collection.forEach(function () {
+      for (var i = 0, j = count; i < j; i++) {
         var div = document.createElement('div');
         div.classList.add('grid-s-12', 'grid-l-6');
         var widget = document.createElement('div');
         widget.classList.add('c-widget');
         div.appendChild(widget);
         fragment.appendChild(div);
-      });
+      }
+
+      return fragment;
+    },
+
+    /**
+     * Render the widgets
+     */
+    _renderWidgets: function () {
+      if (this._loadingError) {
+        this.widgetsContainer.innerHTML = '<p class="loading-error">' +
+          'Unable to load the indicators' +
+          '<button type="button" class="c-button -retry js-retry">Retry</button>' +
+          '</p>';
+        return;
+      }
+
+      var collection = this.indicatorsCollection.toJSON();
+
+      if (!collection.length) {
+        this.widgetsContainer.innerHTML = '';
+        var fragment = this._createWidgetsContainer(4);
+        this.widgetsContainer.appendChild(fragment);
+        return;
+      }
+
+      // We create the containers for the widgets
+      var fragment = this._createWidgetsContainer(collection.length);
 
       this.widgetsContainer.innerHTML = ''; // We ensure it's empty first
       this.widgetsContainer.appendChild(fragment);
