0505519b4206d341ae75896b8b55e97cfec48806
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-03-10 12:22:21 +0100

    Fixes desktop  responsive limit

--- app/assets/javascripts/helpers/responsive.js
+++ app/assets/javascripts/helpers/responsive.js
@@ -1,7 +1,7 @@
 ((function (App) {
   App.Helper.Responsive = {
     isMobile: window.innerWidth <= 375,
-    isTablet: window.innerWidth > 375 && window.innerWidth < 2014,
-    isDesktop: window.innerWidth > 1024
+    isTablet: window.innerWidth > 375 && window.innerWidth < 1024,
+    isDesktop: window.innerWidth >= 1024
   };
 })(this.App));
