438adda7a32954b1ccf51e65dd167b9ffac09bd4
Tiago Santos <tiago.santos@vizzuality.com>
2017-03-14 19:18:53 +0100

    Added Members to the backoffice

---
+++ app/models/member.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id                 :integer          not null, primary key
+#  name               :string
+#  position           :string
+#  organization_name  :string
+#  biography          :text
+#  role               :string
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  slug               :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
+class Member < ApplicationRecord
+  # Validations for paperclip
+  has_attached_file :image, styles: {thumb: '300x300>'}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_enumeration_for :role, with: RoleType, skip_validation: true
+  before_validation :generate_slug
+
+  validates_presence_of :name
+
+  private
+    def generate_slug
+      write_attribute(:slug, self.name.parameterize)
+    end
+end
