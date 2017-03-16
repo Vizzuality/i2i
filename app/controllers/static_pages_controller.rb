53c50bdbd54fe660000b0fab542d3318326987e8
David Inga <david.inga@vizzuality.com>
2017-03-16 13:24:21 +0100

    addin about serializer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,17 @@
 class StaticPagesController < ApplicationController
 
   def about
-    @teamMembers = Member.where(role: 1);
-    @advisoryMembers = Member.where(role: 2);
+    @teamMembers = Member.where(role: 1)
+    @advisoryMembers = Member.where(role: 2)
 
-    gon.team = @teamMembers;
-    gon.advisor = @advisoryMembers;
+    gon.team = serialized(@teamMembers).to_json
+    gon.advisor = serialized(@advisoryMembers).to_json
   end
+
+  private
+
+    def serialized(model)
+      ActiveModelSerializers::SerializableResource.new(model, adapter: :json)
+    end
+
 end
