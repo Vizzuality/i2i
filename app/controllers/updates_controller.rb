dc7bed818f3c13dab535ed2ebd59804bcbc0ec79
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-03-16 10:17:43 +0100

    Sorts By Date

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,8 +1,8 @@
 class UpdatesController < ApplicationController
 
   def index
-    @posts = Blog.limit(4).order(updated_at: :DESC)
-    @news = News.limit(6).order(updated_at: :DESC)
-    @events = Event.limit(4).order(updated_at: :DESC)
+    @posts = Blog.limit(4).order(date: :DESC)
+    @news = News.limit(6).order(date: :DESC)
+    @events = Event.limit(4).order(date: :DESC)
   end
 end
