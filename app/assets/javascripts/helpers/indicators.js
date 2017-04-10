0d507d0e5defc0842a057e4fc6410ffc255babbb
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-04-10 10:00:01 +0100

    Methods to serialize the filters and widgets

--- app/assets/javascripts/helpers/indicators.js
+++ app/assets/javascripts/helpers/indicators.js
@@ -16,6 +16,38 @@
       KEN: 'Kenya',
       MOZ: 'Mozambique',
       PAK: 'Pakistan'
+    },
+
+    /**
+     * Serialize an indicator
+     * @param {{ indicator: string, iso: string, year: number, chart: string, analysisIndicator: {}, compareIndicators: {}[] }} indicator
+     * @return {{ id: string, i: string, y: number, c: string, an: {}, cp: {}[] }}
+     */
+    serialize: function (indicator) {
+      return {
+        id: indicator.indicator,
+        i: indicator.iso,
+        y: indicator.year,
+        c: indicator.chart,
+        an: indicator.analysisIndicator,
+        cp: compareIndicators
+      };
+    },
+
+    /**
+     * Serialize an indicator
+     * @param {{ id: string, i: string, y: number, c: string, an: {}, cp: {}[] }} serializedIndicator
+     * @return {{ indicator: string, iso: string, year: number, chart: string, analysisIndicator: {}, compareIndicators: {}[] }}
+     */
+    deserialize: function (serializedIndicator) {
+      return {
+        indicator: serializedIndicator.id,
+        iso: serializedIndicator.i,
+        year: serializedIndicator.y,
+        chart: serializedIndicator.c,
+        analysisIndicator: serializedIndicator.an,
+        compareIndicators: serializedIndicator.cp
+      };
     }
   };
 })(this.App));
