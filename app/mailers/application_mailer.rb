51e8f75b589aef7d1e878384f6e8d5ce9f3b8d98
Tiago Santos <tiago.santos@vizzuality.com>
2017-04-18 19:27:53 +0200

    First version of the client mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'info@i2i.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,29 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
 
+  require 'sidekiq/web'
+  Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
+  authenticate :admin_user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   root 'homepage#index'
 
   get 'community' => 'community#index'
 
   get 'data-portal' => 'data_portal#index'
-  get 'data-portal/:iso/:year', to: 'data_portal#show', as: 'data_portal_country'
-  get 'data-portal/indicator', to: 'data_portal/indicator#show', as: 'data_portal_indicator'
-  get 'data-portal/report', to: 'data_portal/report#show', as: 'data_portal_report'
+  get 'data-portal/:iso/:year', to: 'data_portal#show',
+                                as: 'data_portal_y'
+  get 'data-portal/indicator', to: 'data_portal/indicator#show',
+                               as: 'data_portal_indicator'
+  get 'data-portal/report', to: 'data_portal/report#show',
+                            as: 'data_portal_report'
 
 
   get 'about' => 'static_pages#about'
   get 'terms-of-use', to: 'static_pages#terms_of_use', as: 'terms_of_use'
 
+  resource :contacts, only: :create
 
   namespace :updates do
     resources :news, only: [:index, :show]
