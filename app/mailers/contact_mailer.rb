0326d867de680b45eb3d4ebf06feb4f1da6458ea
Tiago Santos <tiago.santos@vizzuality.com>
2017-04-19 12:08:48 +0200

    Added real url to the email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,8 @@ class ContactMailer < ApplicationMailer
   default from: 'data@i2i.com'
 
   def data_mail(contact)
-    mail(to: contact.email,
-         subject: "Data for #{contact.country} in #{contact.year}")
+    @contact = contact
+    mail(to: @contact.email,
+         subject: "Data for #{@contact.country} in #{@contact.year}")
   end
 end
