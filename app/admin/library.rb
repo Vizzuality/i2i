877e470458fb1c901e979c65e14f6c99d8231ed2
Ivan Birkman <ivan.yk14@gmail.com>
2017-06-15 13:54:23 +0200

    Add document input to libraries

--- app/admin/library.rb
+++ app/admin/library.rb
@@ -21,7 +21,9 @@ ActiveAdmin.register Library do
     def permitted_params
       params.permit library: [:title, :summary, :id,
                               :image, :date, :url_resource,
-                              :video_url, :subcategory_id, :issuu_link]
+                              :video_url, :subcategory_id, :issuu_link,
+                              documents_attributes: [:file, :name, :id, :_destroy],
+                            	documented_items_attributes: [:document_id, :id, :_destroy]]
     end
   end
 
@@ -57,13 +59,17 @@ ActiveAdmin.register Library do
       f.input :image, as: :file, hint: f.object.image.present? ? \
         image_tag(f.object.image.url(:thumb)) : content_tag(:span, 'No image yet')
       f.input :issuu_link
+      f.has_many :documents, allow_destroy: true, new_record: true, heading: 'Documents' do |a|
+      	a.input :name
+      	a.input :file, as: :file, allow_destroy: true, hint: a.object.file.present? ? \
+        	"#{a.object.file_file_name}" : content_tag(:span, 'No PDF yet')
+      end
       # Will preview the image when the object is edited
       li "Created at #{f.object.created_at}" unless f.object.new_record?
       li "Updated at #{f.object.updated_at}" unless f.object.new_record?
     end
     f.actions
   end
-
 
   show do |ad|
     attributes_table do
@@ -74,6 +80,15 @@ ActiveAdmin.register Library do
       row :title
       row :summary
       row :issuu_link
+      row :documents do
+      	if ad.documents.present?
+					links = ad.documents.map do |document|
+						link_to document.file_file_name, request.base_url + document.file.url
+					end.join(', ')
+
+					raw links
+				end
+      end
       row :image do
         image_tag(ad.image.url(:thumb)) unless ad.image.blank?
       end
