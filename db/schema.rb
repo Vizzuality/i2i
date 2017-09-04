8673136a320bd6f2e4c9ac6d14df4b1fb27b41ec
David Inga <david.inga@vizzuality.com>
2017-09-04 18:34:06 +0200

    header fix on top

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170921085749) do
+=======
+ActiveRecord::Schema.define(version: 20170829174103) do
+>>>>>>> header fix on top
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
