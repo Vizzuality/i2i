4052109375f50a5a0e79e630208160468b23e9f7
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-03 11:14:47 +0200

    Add basic auth for api

--- app/controllers/api/v1/household_member_transactions_controller.rb
+++ app/controllers/api/v1/household_member_transactions_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class HouseholdMemberTransactionsController < ApplicationController
+    class HouseholdMemberTransactionsController < ApiController
       def index
         household_member_transactions = HouseholdMemberTransaction.filter(params.slice(:project_name, :household_name, :category_name, :category_type, :subcategory))
         household_member_transactions = household_member_transactions.page(params[:page])
