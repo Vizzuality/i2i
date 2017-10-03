c9f80e7f5dc49415b35a8769347f4e641f1e7fb3
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-03 12:58:17 +0200

    Remove api namespace

---
+++ app/controllers/category_usages_controller.rb
@@ -0,0 +1,11 @@
+class CategoryUsagesController < ApiController
+  before_action :require_login
+
+  def index
+    category_usages = CategoryUsage.filter(params.slice(:category_type, :category_name, :subcategory))
+    category_usages = category_usages.page(params[:page])
+
+    render json: category_usages, adapter: :json, meta: meta(category_usages)
+  end
+end
+
