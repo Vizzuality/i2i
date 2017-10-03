eaabfa5c82dece77bdd7b981dc4db29d746367c8
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-03 11:14:47 +0200

    Add basic auth for api

--- db/schema.rb
+++ db/schema.rb
@@ -388,6 +388,16 @@ ActiveRecord::Schema.define(version: 20171005162258) do
     t.index ["token"], name: "index_users_on_token", using: :btree
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "token"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["token"], name: "index_users_on_token", using: :btree
+  end
+
   add_foreign_key "libraries", "subcategories"
   add_foreign_key "subcategories", "categories"
 end
