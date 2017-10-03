6ec2f58d99226d2fd38235adc41e27f19007c92e
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-03 12:21:10 +0200

    Uncomment relation

--- app/serializers/household_transaction_serializer.rb
+++ app/serializers/household_transaction_serializer.rb
@@ -19,7 +19,7 @@ class HouseholdTransactionSerializer < ActiveModel::Serializer
   attributes :id, :project_name, :household_name, :category_type, :category_name,
              :subcategory, :num_accounts, :num_members, :num_adults
 
-  # has_many :household_transaction_histories
+  has_many :household_transaction_histories
 
   def household_transaction_histories
     object.household_transaction_histories.order(year: :asc, month: :asc)
