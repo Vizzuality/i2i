3ca57fd0a535c156b79824bce5091ec13c0466d6
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-03 13:20:59 +0200

    Add per page

--- app/controllers/household_member_transactions_controller.rb
+++ app/controllers/household_member_transactions_controller.rb
@@ -3,7 +3,7 @@ class HouseholdMemberTransactionsController < ApiController
 
   def index
     household_member_transactions = HouseholdMemberTransaction.filter(params.slice(:project_name, :household_name, :category_name, :category_type, :subcategory))
-    household_member_transactions = household_member_transactions.page(params[:page])
+    household_member_transactions = household_member_transactions.page(params[:page]).per(params[:per_page])
 
     render json: household_member_transactions, adapter: :json, meta: meta(household_member_transactions)
   end
