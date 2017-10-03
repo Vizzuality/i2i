e24c1d895f57969beeff133b54a7afbccb35cae0
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-03 12:40:28 +0200

    Fix render json syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string
+#  token           :string
+#  email           :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   has_secure_password
   has_secure_token
