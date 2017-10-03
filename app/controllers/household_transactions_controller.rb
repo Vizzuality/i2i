402f0d78d175c7b38f9752343ecd30d8b8a6e3f4
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-03 12:58:17 +0200

    Remove api namespace

---
+++ app/controllers/household_transactions_controller.rb
@@ -0,0 +1,10 @@
+class HouseholdTransactionsController < ApiController
+  before_action :require_login
+
+  def index
+    household_transactions = HouseholdTransaction.filter(params.slice(:project_name, :household_name, :category_name, :category_type, :subcategory))
+    household_transactions = household_transactions.page(params[:page])
+
+    render json: household_transactions, adapter: :json, meta: meta(household_transactions)
+  end
+end
---
+++ app/controllers/project_metadata_controller.rb
@@ -0,0 +1,10 @@
+class ProjectMetadataController < ApiController
+  before_action :require_login
+
+  def index
+    project_metadata = ProjectMetadatum.filter(params.slice(:project_name))
+    project_metadata = project_metadata.page(params[:page])
+
+    render json: project_metadata, adapter: :json, meta: meta(project_metadata)
+  end
+end
