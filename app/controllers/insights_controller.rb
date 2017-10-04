5df01348f7f8f729c1284f8577f37bdacfa2167f
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-04 16:27:53 +0200

    Add basic show for insights

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -8,7 +8,7 @@ class InsightsController < ApplicationController
     records << Event.where(published: true)
     records << Library.where(published: true)
 
-    @records = records.flatten.sort { |a, b| b[:created_at] <=> a[:created_at] }
+    @insights = records.flatten.sort { |a, b| b[:created_at] <=> a[:created_at] }
     @categories = Category.all
 
     # news = News.where(published: true).where("title LIKE ? OR summary LIKE ? OR content LIKE ?", "%#{term}%", "%#{term}%", "%#{term}%")
@@ -17,9 +17,7 @@ class InsightsController < ApplicationController
     # libraries = Library.where(published: true).where("title LIKE ? OR summary LIKE ?", "%#{term}%", "%#{term}%")
   end
 
-  def filter_param
-    [:tag, :subcategory, :category].each do |permitted|
-      return params.permit(permitted) if params[permitted].present?
-    end
+  def show
+    @insight = params[:entity].capitalize.constantize.published.friendly.find(params[:slug])
   end
 end
