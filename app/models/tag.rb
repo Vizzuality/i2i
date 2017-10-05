eead503b824010cc9cfc8cf6c1e5c4c39a42826e
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-05 17:26:31 +0200

    Add featured tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -19,6 +19,10 @@ class Tag < ApplicationRecord
   has_many :blogs, :through => :tagged_items, :source => :taggable, :source_type => 'Blog'
   has_many :news, :through => :tagged_items, :source => :taggable, :source_type => 'News'
   validates :name, uniqueness: { case_sensitive: false }
+  validates_presence_of :image_url, :if => :is_featured?, message: "must be present if tag is featured"
+  validates_presence_of :description, :if => :is_featured?, message: "must be present if tag is featured"
+
+  scope :featured, -> (featured) { where is_featured: featured }
 
   def should_generate_new_friendly_id?
     name_changed?
