5e0fd7951325e8c954a96c054c81b52dd197b38c
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-10-09 17:37:01 +0200

    Implements new data portal navigation

--- app/assets/javascripts/pages/data_portal/DataPortalIndexPage.js
+++ app/assets/javascripts/pages/data_portal/DataPortalIndexPage.js
@@ -3,46 +3,60 @@
 
   App.Page.DataPortalIndexPage = Backbone.View.extend({
 
-    el: '.js-countries',
+    el: 'body',
 
-    template: JST['templates/data_portal/index-page'],
-
-    initialize: function () {
-      this.collection = new App.Collection.CountriesCollection();
-      this._fetchData();
+    events: {
+      'click .js-anchor': '_onClickAnchor'
     },
+
+    // template: JST['templates/data_portal/index-page'],
+
+    // initialize: function () {
+    //   this.collection = new App.Collection.CountriesCollection();
+    //   this._fetchData();
+    // },
 
     /**
      * Fetch the Collection
      */
-    _fetchData: function () {
-      // The first time we render to have the placeholder flags
-      this.render();
+    // _fetchData: function () {
+    //   // The first time we render to have the placeholder flags
+    //   this.render();
 
-      this.collection.fetch()
-        .done(function () {
-          this._loadingError = false;
-        }.bind(this))
-        .fail(function () {
-          this._loadingError = true;
-        }.bind(this))
-        .always(this.render.bind(this));
-    },
+    //   this.collection.fetch()
+    //     .done(function () {
+    //       this._loadingError = false;
+    //     }.bind(this))
+    //     .fail(function () {
+    //       this._loadingError = true;
+    //     }.bind(this))
+    //     .always(this.render.bind(this));
+    // },
 
-    render: function () {
-      if (this._loadingError) {
-        new App.View.RetryMessageView({
-          el: this.el,
-          label: 'Unable to load the countries',
-          callback: this._fetchData.bind(this)
-        });
-        return;
-      }
+    _onClickAnchor: function (e) {
+      e.preventDefault();
+      var target = e.currentTarget.getAttribute('href');
 
-      this.el.innerHTML = this.template({
-        countries: this.collection.toJSON()
+      $('html, body').animate({
+        scrollTop: $(target).offset().top
       });
     }
+
+    // render: function () {
+    //   if (this._loadingError) {
+    //     new App.View.RetryMessageView({
+    //       el: this.$el.find('.js-countries'),
+    //       label: 'Unable to load the countries',
+    //       callback: this._fetchData.bind(this)
+    //     });
+    //     return;
+    //   }
+
+
+    //   this.$el.find('.js-countries').html(this.template({
+    //     countries: this.collection.toJSON()
+    //   }));
+    // }
 
   });
 
