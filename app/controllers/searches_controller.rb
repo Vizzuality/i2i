f9fdb82caada2dfeecdb2a529a4b205cda1684f2
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-10 19:09:51 +0200

    Add realtions as searchable fields for global search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,10 +2,26 @@ class SearchesController < ApplicationController
   def index
     term = params[:term]
 
-    news = News.where(published: true).where("title LIKE ? OR summary LIKE ? OR content LIKE ?", "%#{term}%", "%#{term}%", "%#{term}%")
-    blogs = Blog.where(published: true).where("title LIKE ? OR summary LIKE ? OR content LIKE ?", "%#{term}%", "%#{term}%", "%#{term}%")
-    events = Event.where(published: true).where("title LIKE ? OR summary LIKE ? OR content LIKE ?", "%#{term}%", "%#{term}%", "%#{term}%")
-    libraries = Library.where(published: true).where("title LIKE ? OR summary LIKE ?", "%#{term}%", "%#{term}%")
+    news = News.where(published: true)
+               .joins(:category)
+               .joins(:tags)
+               .where("lower(title) LIKE ? OR lower(summary) LIKE ? OR lower(content) LIKE ? OR lower(categories.name) like ? OR lower(tags.name) ?",
+                      "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%")
+    blogs = Blog.where(published: true)
+                .joins(:category)
+                .joins(:tags)
+                .where("lower(title) LIKE ? OR lower(summary) LIKE ? OR lower(content) LIKE ? OR lower(categories.name) like ? OR lower(tags.name) ?",
+                       "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%")
+    events = Event.where(published: true)
+                  .joins(:category)
+                  .joins(:tags)
+                  .where("lower(title) LIKE ? OR lower(summary) LIKE ? OR lower(content) LIKE ? OR lower(categories.name) like ? OR lower(tags.name) ?",
+                         "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%")
+    libraries = Library.where(published: true)
+                       .joins(:category)
+                       .joins(:tags)
+                       .where("lower(title) LIKE ? OR lower(summary) LIKE ? OR lower(categories.name) like ? OR lower(tags.name) ?",
+                              "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%", "%#{term.downcase}%")
 
     @categories = Category.all
     @records = (news + blogs + events + libraries)
