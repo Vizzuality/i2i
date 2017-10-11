a6df0704e8ca033207c5fb47d5bcb547a826cbb7
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-11 18:13:08 +0200

    Pull origin

--- app/controllers/data_portal_financial_diaries_controller.rb
+++ app/controllers/data_portal_financial_diaries_controller.rb
@@ -2,7 +2,7 @@ class DataPortalFinancialDiariesController < ApplicationController
   def index
     @country = Country.find_by(iso: params[:iso])
     @categories = CategoryUsage.categories_with_children
-    @project_quantities = ProjectMetadatum.quantities
+    @project_quantities = ProjectMetadatum.quantities(country_iso)
 
     gon.categories = JSON.parse @categories.to_json
   end
