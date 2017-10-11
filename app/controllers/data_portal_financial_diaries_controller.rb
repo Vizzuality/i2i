f4cbe87a102911ae2c81d1860c94ea05b5b2bec4
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-12 00:13:29 +0200

    Add first version of household transactions filter

--- app/controllers/data_portal_financial_diaries_controller.rb
+++ app/controllers/data_portal_financial_diaries_controller.rb
@@ -3,6 +3,31 @@ class DataPortalFinancialDiariesController < ApplicationController
     @country = Country.find_by(iso: params[:iso])
     @categories = CategoryUsage.categories_with_children
     @project_quantities = ProjectMetadatum.quantities(country_iso)
+    @transactions = []
+
+    if params[:p].present?
+      filters = JSON.parse(Base64.decode64(params[:p]))
+
+      if filters['type'] == 'households'
+        filters['categories'].each do |category|
+          if category['subcategory']
+            @transactions << HouseholdTransaction.where(project_name: project_name, category_type: category['type'], subcategory: category['subcategory'])
+          else
+            @transactions << HouseholdTransaction.where(project_name: project_name, category_type: category['type'])
+          end
+        end
+      else
+        filters['categories'].each do |category|
+          if category['subcategory']
+            @transactions << HouseholdMemberTransaction.where(project_name: project_name, category_type: category['type'], subcategory: category['subcategory'])
+          else
+            @transactions << HouseholdMemberTransaction.where(project_name: project_name, category_type: category['type'])
+          end
+        end
+      end
+    end
+
+    @transactions.flatten!
 
     gon.categories = JSON.parse @categories.to_json
   end
