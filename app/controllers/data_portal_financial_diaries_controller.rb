e622b2e7b98e6eb7cd44908827103d3781b273d3
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-10-11 18:11:57 +0200

    Sets default category and type

--- app/controllers/data_portal_financial_diaries_controller.rb
+++ app/controllers/data_portal_financial_diaries_controller.rb
@@ -5,5 +5,7 @@ class DataPortalFinancialDiariesController < ApplicationController
     @country = FinscopeApi.get_countries.find{ |c| c[:iso] == params[:iso] }
     @categories = CategoryUsage.categories_with_children
     @project_quantities = ProjectMetadatum.quantities
+
+    gon.categories = JSON.parse @categories.to_json
   end
 end
