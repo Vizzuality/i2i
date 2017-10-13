3897ad9d2a6b57488a3a3850f97080c0648cc66d
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-13 13:33:02 +0200

    Change data portal countries list

--- app/models/country.rb
+++ app/models/country.rb
@@ -10,5 +10,30 @@
 #
 
 class Country < ApplicationRecord
+  include FinscopeApi
 
+  attr_accessor :has_finscope
+  attr_accessor :has_financial_diaries
+
+  class << self
+    def country_list
+      countries = all
+      countries.each do |country|
+        iso = country.iso
+        country.has_finscope = finscope_country(iso)
+        country.has_financial_diaries = financial_diaries_country(iso)
+      end
+
+      countries
+    end
+
+    def finscope_country(iso)
+      FinscopeApi.get_countries.select { |fc| fc[:iso] == iso }.first
+    end
+
+    def financial_diaries_country(iso)
+      country = ProjectMetadatum.find_by(country_iso3: iso)
+      country.attributes if country
+    end
+  end
 end
