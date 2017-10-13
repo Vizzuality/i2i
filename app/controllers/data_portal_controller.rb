caff876ee32918ce76dd6b952706f4d06b715d6a
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-13 14:29:45 +0200

    Show country if has any dataset, not only finscope

--- app/controllers/data_portal_controller.rb
+++ app/controllers/data_portal_controller.rb
@@ -2,12 +2,11 @@ class DataPortalController < ApplicationController
   include FinscopeApi
 
   def index
-    # @countries = FinscopeApi.get_countries
     @countries = Country.country_list
   end
 
   def country_preview
-    @country = FinscopeApi.get_countries.find{ |c| c[:iso] == params[:iso] }
+    @country = Country.find_by(iso: params[:iso])
   end
 
   def show
