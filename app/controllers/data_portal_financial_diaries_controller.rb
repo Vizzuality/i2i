d78b58eb2f9ffcfb879efbceaabb68a8024c3f9f
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-17 17:11:11 +0200

    On financial diaries, look for coutries from database, not from finscope api

--- app/controllers/data_portal_financial_diaries_controller.rb
+++ app/controllers/data_portal_financial_diaries_controller.rb
@@ -1,8 +1,6 @@
 class DataPortalFinancialDiariesController < ApplicationController
-  include FinscopeApi
-
   def index
-    @country = FinscopeApi.get_countries.find{ |c| c[:iso] == params[:iso] }
+    @country = Country.find_by(iso: params[:iso])
     @categories = CategoryUsage.categories_with_children
     @project_quantities = ProjectMetadatum.quantities
   end
