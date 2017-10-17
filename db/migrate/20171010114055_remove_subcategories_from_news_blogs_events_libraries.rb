32fea1e3620beae9755e879acf20f82cfc5902d6
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-17 11:08:28 +0200

    Fix migrations problem

--- db/migrate/20171010114055_remove_subcategories_from_news_blogs_events_libraries.rb
+++
@@ -1,8 +0,0 @@
-class RemoveSubcategoriesFromNewsBlogsEventsLibraries < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :blogs, :subcategory_id
-    remove_column :events, :subcategory_id
-    remove_column :news, :subcategory_id
-    remove_column :libraries, :subcategory_id
-  end
-end
