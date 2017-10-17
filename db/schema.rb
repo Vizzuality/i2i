4709f294a7c5a01130a78ecfc8c44967ed676fde
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-17 15:55:24 +0200

    Add member quantity to project metadata

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20171017134349) do
-=======
-ActiveRecord::Schema.define(version: 20171013113717) do
->>>>>>> Add display tag title
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
