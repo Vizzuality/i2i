a4bbd58e56bb3179065023aad7c5dce12aaa23f0
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-17 15:55:24 +0200

    Add member quantity to project metadata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013113717) do
+ActiveRecord::Schema.define(version: 20171017134349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -308,6 +308,7 @@ ActiveRecord::Schema.define(version: 20171013113717) do
     t.datetime "end_date"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "num_members_in_mem"
   end
 
   create_table "sessions", force: :cascade do |t|
