fb055504e281ae72698e9fb0618ab2593a785751
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-19 12:16:29 +0200

    Redirect to blogs when no insights category is sent

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -19,18 +19,13 @@ class InsightsController < ApplicationController
 
     @total_insights = totals[@category.name]
 
-    if params[:category].present?
-      if @category.present?
-        entities.each { |klass| records << klass.where(published: true) }
-        insights = records.flatten.sort { |a, b| b[:date] <=> a[:date] }
-        @insights = insights.select { |r| r.category_id == @category.id }.take(page_quantity)
-      else
-        @insights = []
-      end
+    if @category.present?
+      entities.each { |klass| records << klass.where(published: true) }
+      insights = records.flatten.sort { |a, b| b[:date] <=> a[:date] }
+      @insights = insights.select { |r| r.category_id == @category.id }.take(page_quantity)
     else
-      redirect_to insights_filter_index_path(Category.first) rescue nil
+      @insights = []
     end
-
 
     if params[:offset].present?
       @morePaginationAvailable = (@total_insights - (params[:offset].to_i * ENV['OFFSET_SIZE'].to_i)) > 0
@@ -39,5 +34,9 @@ class InsightsController < ApplicationController
     end
 
     @offset = params[:offset] ? params[:offset].to_i + 1 : 2;
+
+    rescue
+      category = Category.find_by(slug: 'blogs') || Category.first
+      redirect_to insights_filter_index_path(category) rescue nil
   end
 end
