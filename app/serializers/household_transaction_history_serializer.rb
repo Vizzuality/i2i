f2cb4eade1ac5357bd6a01f02d7c7ce44f164e6b
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-20 17:09:59 +0200

    Remove comments

--- app/serializers/household_transaction_history_serializer.rb
+++ app/serializers/household_transaction_history_serializer.rb
@@ -21,21 +21,6 @@ class HouseholdTransactionHistorySerializer < ActiveModel::Serializer
   def avg_value
     if object.value.present?
       object.value.split(':')[1].to_f
-
-      # values = object.value.split(':').map { |val| val.eql?("null") ? nil : val.to_f }
-
-      # {
-      #   total_transaction_value: values[0],
-      #   avg_value: values[1],
-      #   min_value: values[2],
-      #   max_value: values[3],
-      #   rolling_balance: values[4],
-      #   business_expenses: values[5],
-      #   withdrawals: values[6],
-      #   deposits: values[7],
-      #   new_borrowing: values[8],
-      #   repayment: values[9]
-      # }
     end
   end
 end
