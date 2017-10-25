9b45eecb15add5f0e0865585fd936d53067c62a8
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-25 12:45:34 +0200

    Low level cache for api

--- app/controllers/fdapi/project_metadata_controller.rb
+++ app/controllers/fdapi/project_metadata_controller.rb
@@ -10,44 +10,50 @@ module Fdapi
     end
 
     def project_min_max
-      project_metadata = ProjectMetadatum.find_by(project_name: params[:project_name])
       categories_filter = JSON.parse(params[:categories])
+      cache_key = "project_min_max-#{params.slice(:project_name, :household_name).to_json}-#{categories_filter}"
 
-      household_transactions = categories_filter.map do |category|
-        HouseholdTransaction.filter(params.slice(:project_name, :household_name).merge(category))
-      end.flatten
+      data = Rails.cache.fetch(cache_key) do
+        project_metadata = ProjectMetadatum.find_by(project_name: params[:project_name])
 
-      adapter = ActiveRecord::Base.connection
+        household_transactions = categories_filter.map do |category|
+          HouseholdTransaction.filter(params.slice(:project_name, :household_name).merge(category))
+        end.flatten
 
-      sql_result = adapter.execute("select household_transactions.category_type,
-          min(household_transaction_histories.rolling_balance), min(total_transaction_value),
-          max(household_transaction_histories.rolling_balance), max(total_transaction_value)
-          from household_transaction_histories inner join household_transactions
-          on household_transactions.id = household_transaction_histories.household_transaction_id
-          where household_transactions.id in(#{household_transactions.pluck(:id).join(', ')})
-          group by household_transactions.category_type")
+        adapter = ActiveRecord::Base.connection
 
-      data = []
-      field_index =
-        { category: 0, min_rolling: 1, min_total: 2, max_rolling: 3, max_total: 4}
+        sql_result = adapter.execute("select household_transactions.category_type,
+            min(household_transaction_histories.rolling_balance), min(total_transaction_value),
+            max(household_transaction_histories.rolling_balance), max(total_transaction_value)
+            from household_transaction_histories inner join household_transactions
+            on household_transactions.id = household_transaction_histories.household_transaction_id
+            where household_transactions.id in(#{household_transactions.pluck(:id).join(', ')})
+            group by household_transactions.category_type")
 
-      sql_result.values.each do |values|
-        case values[field_index[:category]]
-        when 'credits', 'savings'
-          data << json_element('min', values[field_index[:min_rolling]],
-                               values[field_index[:category]],
-                               project_metadata.start_date.strftime('%Y-%m-%d'))
-          data << json_element('max', values[field_index[:max_rolling]],
-                               values[field_index[:category]],
-                               project_metadata.end_date.strftime('%Y-%m-%d'))
-        when 'expense', 'income'
-          data << json_element('min', values[field_index[:min_total]],
-                               values[field_index[:category]],
-                               project_metadata.start_date.strftime('%Y-%m-%d'))
-          data << json_element('max', values[field_index[:max_total]],
-                               values[field_index[:category]],
-                               project_metadata.end_date.strftime('%Y-%m-%d'))
+        data = []
+        field_index =
+          { category: 0, min_rolling: 1, min_total: 2, max_rolling: 3, max_total: 4}
+
+        sql_result.values.each do |values|
+          case values[field_index[:category]]
+          when 'credits', 'savings'
+            data << json_element('min', values[field_index[:min_rolling]],
+                                 values[field_index[:category]],
+                                 project_metadata.start_date.strftime('%Y-%m-%d'))
+            data << json_element('max', values[field_index[:max_rolling]],
+                                 values[field_index[:category]],
+                                 project_metadata.end_date.strftime('%Y-%m-%d'))
+          when 'expense', 'income'
+            data << json_element('min', values[field_index[:min_total]],
+                                 values[field_index[:category]],
+                                 project_metadata.start_date.strftime('%Y-%m-%d'))
+            data << json_element('max', values[field_index[:max_total]],
+                                 values[field_index[:category]],
+                                 project_metadata.end_date.strftime('%Y-%m-%d'))
+          end
         end
+
+        data
       end
 
 
@@ -77,52 +83,58 @@ module Fdapi
     end
 
     def project_means
-      project_metadata = ProjectMetadatum.find_by(project_name: params[:project_name])
       categories_filter = JSON.parse(params[:categories])
-      adapter = ActiveRecord::Base.connection
-      histories = {}
-      result = []
+      cache_key = "project_means-#{params.slice(:project_name, :household_name).to_json}-#{categories_filter}"
 
-      household_transactions = categories_filter.map do |category|
-        { category['category_type'] => HouseholdTransaction.filter(params.slice(:project_name, :household_name).merge(category)) }
-      end
+      result = Rails.cache.fetch(cache_key) do
+        project_metadata = ProjectMetadatum.find_by(project_name: params[:project_name])
+        adapter = ActiveRecord::Base.connection
+        histories = {}
+        result = []
 
-      household_transactions.each do |household|
-        category_type = household.keys.first
+        household_transactions = categories_filter.map do |category|
+          { category['category_type'] => HouseholdTransaction.filter(params.slice(:project_name, :household_name).merge(category)) }
+        end
 
-        histories[category_type] = adapter.execute("select * from household_transaction_histories
-          where household_transaction_id in (#{household.values.first.pluck(:id).join(', ')})
-          and #{default_indicators[category_type]} is not null")
-      end
+        household_transactions.each do |household|
+          category_type = household.keys.first
 
-      sorted = {}
-      histories.each do |category_type, pg_result|
-        scores = {}
-        pg_result.to_a.each { |history| (scores[history['year']] ||= {})[history['month']]=[] }
-        sorted[category_type] = scores
-      end
+          histories[category_type] = adapter.execute("select * from household_transaction_histories
+            where household_transaction_id in (#{household.values.first.pluck(:id).join(', ')})
+            and #{default_indicators[category_type]} is not null")
+        end
 
-      histories.each do |category_type, pg_result|
-        pg_result.to_a.each { |history| sorted[category_type][history['year']][history['month']] << history }
-      end
+        sorted = {}
+        histories.each do |category_type, pg_result|
+          scores = {}
+          pg_result.to_a.each { |history| (scores[history['year']] ||= {})[history['month']]=[] }
+          sorted[category_type] = scores
+        end
 
-      sorted.each do |category_type, value|
-        value.each do |year, value|
-          value.each do |month, histories|
-            values = histories.map { |h| h[default_indicators[category_type]] }
+        histories.each do |category_type, pg_result|
+          pg_result.to_a.each { |history| sorted[category_type][history['year']][history['month']] << history }
+        end
 
-            result << {
-              category_type: category_type,
-              max: values.max,
-              min: values.min,
-              median: values.sum / values.size.to_f,
-              date: "#{year}-#{month.to_s.rjust(2, '0')}-01"
-            }
+        sorted.each do |category_type, value|
+          value.each do |year, value|
+            value.each do |month, histories|
+              values = histories.map { |h| h[default_indicators[category_type]] }
+
+              result << {
+                category_type: category_type,
+                max: values.max,
+                min: values.min,
+                median: values.sum / values.size.to_f,
+                date: "#{year}-#{month.to_s.rjust(2, '0')}-01"
+              }
+            end
           end
         end
+
+        result.sort_by { |h| h[:date] }
       end
 
-      render json: { data: result.sort_by { |h| h[:date] } }
+      render json: { data: result }
     end
 
     def json_element(operation, value, category, date)
