8bededfbab0d636285d5b5ca03ccefb661b0d3cf
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-25 12:45:34 +0200

    Low level cache for api

--- app/controllers/fdapi/household_transactions_controller.rb
+++ app/controllers/fdapi/household_transactions_controller.rb
@@ -1,12 +1,14 @@
 module Fdapi
   class HouseholdTransactionsController < ApiController
-
     def index
       categories_filter = JSON.parse(params[:categories])
+      cache_key = "household_transactions-#{params.slice(:project_name, :household_name).to_json}-#{categories_filter}"
 
-      household_transactions = categories_filter.map do |category|
-        HouseholdTransaction.filter(params.slice(:project_name, :household_name).merge(category)).includes(:household_transaction_histories)
-      end.flatten
+      household_transactions = Rails.cache.fetch(cache_key) do
+        categories_filter.map do |category|
+          HouseholdTransaction.filter(params.slice(:project_name, :household_name).merge(category)).includes(:household_transaction_histories)
+        end.flatten
+      end
 
       render json: household_transactions, adapter: :json, root: 'data'
     end
@@ -15,37 +17,41 @@ module Fdapi
       project_name = params[:project_name]
       household_name = params[:household]
       category_types = JSON.parse(params[:categories]).map { |cat| cat['category_type'] }
-      response = []
+      cache_key = "monthly_values-#{project_name}-#{household_name}-#{category_types}"
 
-      category_types.each do |category_type|
-        transactions = HouseholdTransaction
-                        .with_subcategory
-                        .project_name(project_name)
-                        .category_type(category_type)
+      response = Rails.cache.fetch(cache_key) do
+        response = []
 
-        if household_name.present?
-          transactions = transactions.household_name(household_name)
+        category_types.each do |category_type|
+          transactions = HouseholdTransaction
+                          .with_subcategory
+                          .project_name(project_name)
+                          .category_type(category_type)
+
+          if household_name.present?
+            transactions = transactions.household_name(household_name)
+          end
+
+          grouped = transactions.group_by { |transaction| transaction.subcategory }
+
+          grouped.each do |subcategory, transactions|
+            indicator = default_indicators[category_type]
+
+            histories = transactions.map do |transaction|
+              transaction.household_transaction_histories_all.with_indicator(indicator)
+            end
+            .flatten
+            .group_by { |hist| "#{hist.year}-#{hist.month.to_s.rjust(2, '0')}-01" }
+
+            histories.each do |date, histories|
+              value = histories.pluck(indicator).compact.reduce(:+)
+              response << monthly_json(subcategory, date, value, category_type)
+            end
+          end
         end
 
-        grouped = transactions.group_by { |transaction| transaction.subcategory }
-
-        grouped.each do |subcategory, transactions|
-          indicator = default_indicators[category_type]
-
-          histories = transactions.map do |transaction|
-            transaction.household_transaction_histories_all.with_indicator(indicator)
-          end
-          .flatten
-          .group_by { |hist| "#{hist.year}-#{hist.month.to_s.rjust(2, '0')}-01" }
-
-          histories.each do |date, histories|
-            value = histories.pluck(indicator).compact.reduce(:+)
-            response << monthly_json(subcategory, date, value, category_type)
-          end
-        end
+        response.sort_by! { |v| v[:date] }
       end
-
-      response.sort_by! { |v| v[:date] }
 
       render json: { data: response }
     end
