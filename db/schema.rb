c74aa4b47c760c2b449c275f891909419fe072df
Clara Linos <clara.linos@gmail.com>
2017-10-25 11:42:33 +0200

    db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20171019145000) do
     t.string   "slug"
     t.boolean  "published"
     t.string   "custom_author"
+    t.integer  "subcategory_id"
     t.string   "record_type",        default: "blog"
     t.integer  "category_id"
     t.boolean  "is_featured",        default: false
@@ -152,6 +153,7 @@ ActiveRecord::Schema.define(version: 20171019145000) do
     t.string   "slug"
     t.boolean  "published"
     t.string   "custom_author"
+    t.integer  "subcategory_id"
     t.string   "record_type",        default: "event"
     t.integer  "category_id"
     t.boolean  "is_featured",        default: false
@@ -309,6 +311,7 @@ ActiveRecord::Schema.define(version: 20171019145000) do
     t.string   "issuu_link"
     t.string   "slug"
     t.boolean  "published"
+    t.integer  "subcategory_id"
     t.string   "record_type",        default: "news"
     t.integer  "category_id"
     t.boolean  "is_featured",        default: false
