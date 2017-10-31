4498a3dd585b91331cc45ecc41845cb2e5859eeb
Ivan Birkman <ivan.yk14@gmail.com>
2017-10-31 16:10:25 +0100

    Move filter variables back to controller

--- app/controllers/data_portal_financial_diaries_controller.rb
+++ app/controllers/data_portal_financial_diaries_controller.rb
@@ -19,6 +19,55 @@ class DataPortalFinancialDiariesController < ApplicationController
       members: MemberSubcategoryIncome.main_incomes(project_name)
     }
 
+    @gender_options = [
+      {
+        name: 'Male',
+        value: 'male'
+      },
+      {
+        name: 'Female',
+        value: 'female'
+      }
+    ]
+
+    @age_options = [
+      {
+        name: '18-25',
+        value: {
+          min_age: 18,
+          max_age: 25
+        }
+      },
+      {
+        name: '25-35',
+        value: {
+          min_age: 25,
+          max_age: 35
+        }
+      },
+      {
+        name: '35-45',
+        value: {
+          min_age: 35,
+          max_age: 45
+        }
+      },
+      {
+        name: '45-60',
+        value: {
+          min_age: 45,
+          max_age: 60
+        }
+      },
+      {
+        name: '>60',
+        value: {
+          min_age: 60,
+          max_age: 200
+        }
+      }
+    ]
+
     @filters = [];
 
     if params[:p].present?
