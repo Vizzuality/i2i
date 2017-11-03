b6c25d148f2c79302f139fa0d11ae7e06b47e1dd
David Inga <david.inga@vizzuality.com>
2017-11-03 17:20:04 +0100

    fixing tooltip

--- app/assets/javascripts/views/financial_diaries/visualisations/MainChartHouseholdView.js
+++ app/assets/javascripts/views/financial_diaries/visualisations/MainChartHouseholdView.js
@@ -6,22 +6,22 @@
     el: '#vis-main-household-detail-chart',
 
     options: {
-      customTooltip: true,
-      customTooltipOptions: {
-        fields: [
-          {
-            title: 'Value',
-            value: 'value',
-            formatType: 'number',
-            format: "s"
-          },
-          {
-            title: 'Date',
-            field: 'date',
-            formatType: 'time',
-            format: '%Y-%m'
-          }
-        ]
+      tooltip: {
+        showAllFields: false,
+        fields: [{
+          title: 'Subcategory',
+          field: 'subcategory'
+        }, {
+          title: 'Value',
+          field: 'value',
+          formatType: 'number',
+          format: 's'
+        }, {
+          title: 'Date',
+          field: 'date',
+          formatType: 'time',
+          format: '%Y-%m'
+        }]
       },
       spec: App.Specs.MainChartHousehold
     }
