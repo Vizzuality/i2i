e91499a894904c3cdc9f30b8cabbd8e666482035
Ivan Birkman <ivan.yk14@gmail.com>
2018-01-03 11:26:43 +0100

    Add search with multiple terms

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
   def index
-    term = params[:term]
+    term = params[:term].split(',').map(&:strip) rescue nil
 
     if !term || term.empty?
 
@@ -15,15 +15,18 @@ class SearchesController < ApplicationController
         [[a[:position] ? 0 : 1, a[:position]], b[:updated_at]] <=> [[b[:position] ? 0 : 1, b[:position]], a[:updated_at]]
       end[0..5]
     else
+      search_result = []
 
-      news = News.search_fields(term)
-      blogs = Blog.search_fields(term)
-      events = Event.search_fields(term)
-      libraries = Library.search_fields(term)
+      term.each do |search_term|
+        search_result << News.search_fields(search_term)
+        search_result << Blog.search_fields(search_term)
+        search_result << Event.search_fields(search_term)
+        search_result << Library.search_fields(search_term)
+      end
 
       @tags = Tag.all
       @categories = Category.all
-      @records = (news + blogs + events + libraries)
+      @records = (search_result.flatten)
     end
   end
 end
