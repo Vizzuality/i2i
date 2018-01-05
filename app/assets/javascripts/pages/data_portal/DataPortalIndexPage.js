f4fe5bb6aeb4a220c909be6bab58377a84c937a2
Ivan Birkman <ivan.yk14@gmail.com>
2018-01-05 16:02:36 +0100

    Add option to remove dataset filter on countries

--- app/assets/javascripts/pages/data_portal/DataPortalIndexPage.js
+++ app/assets/javascripts/pages/data_portal/DataPortalIndexPage.js
@@ -8,7 +8,8 @@
     events: {
       'click .js-anchor': '_onClickAnchor',
       'click .filter-finscope': 'showFinscope',
-      'click .filter-financial-diaries': 'showFinancialDiaries'
+      'click .filter-financial-diaries': 'showFinancialDiaries',
+      'click .see-all-countries': 'showAllCountries'
     },
 
     _onClickAnchor: function (e) {
@@ -22,14 +23,39 @@
 
     showFinscope: function() {
       var list = this.$el.find('.l-country-list');
+      var selectDescription = this.$el.find('.select-country-description')[0];
+
       list.addClass("is-finscope");
       list.removeClass("is-financial-diaries");
+
+      selectDescription.innerHTML = "National Surveys countries - \
+                                     Please click on the country’s flag below to view \
+                                     the datasets available for that country. \
+                                     <a href='.l-country-list' class='see-all-countries js-anchor'>See all countries</a>";
     },
 
     showFinancialDiaries: function() {
       var list = this.$el.find('.l-country-list');
+      var selectDescription = this.$el.find('.select-country-description')[0];
+
       list.addClass("is-financial-diaries");
       list.removeClass("is-finscope");
+
+      selectDescription.innerHTML = "Financial Diaries countries - \
+                                     Please click on the country’s flag below to view \
+                                     the datasets available for that country. \
+                                     <a href='.l-country-list' class='see-all-countries js-anchor'>See all countries</a>";
+    },
+
+    showAllCountries: function() {
+      var list = this.$el.find('.l-country-list');
+      var selectDescription = this.$el.find('.select-country-description')[0];
+
+      list.removeClass("is-finscope");
+      list.removeClass("is-financial-diaries");
+
+      selectDescription.innerHTML = 'Please click on the country’s flag below to \
+                                     view the datasets available for that country.'
     }
   });
 
