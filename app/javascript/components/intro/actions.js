10059f5260bb8f24ae73c26da9b88b6b27822a61
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-07-17 15:58:44 +0200

    Sql supported

--- app/javascript/components/intro/actions.js
+++ app/javascript/components/intro/actions.js
@@ -1,32 +1,41 @@
 import { createAction, createThunkAction } from 'redux-tools';
 
+// SQL
+import INTRO_SQL from './sql/intro.sql';
+
 export const setIntro = createAction('INTRO/setIntro');
 export const setIntroLoading = createAction('INTRO/setIntroLoading');
 export const setIntroError = createAction('INTRO/setIntroError');
 
-export const fetchIntro = createThunkAction('INTRO/fetchIntro', () => (dispatch) => {
+export const fetchIntro = createThunkAction('INTRO/fetchIntro', () => (dispatch, getState) => {
+  const { replace } = window.App.Helper.Utils;
+
   dispatch(setIntroLoading(true));
 
   // return fetch(new Request(`${process.env.API_URL}/`))
-  //   .then((response) => {
-  //     if (response.ok) return response.json();
-  //     throw new Error(response.statusText);
-  //   })
-  //   .then((data) => {
-  //     dispatch(setIntroLoading(false));
-  //     dispatch(setIntroError(null));
+  return fetch(`https://fsp-maps.carto.com/api/v2/sql?q=${encodeURIComponent(replace(INTRO_SQL, { iso: 'KEN' }))}&api_key=8kFyv1NsG2qllfDd972LJQ`)
+    .then((response) => {
+      if (response.ok) return response.json();
+      throw new Error(response.statusText);
+    })
+    .then((data) => {
+      dispatch(setIntroLoading(false));
+      dispatch(setIntroError(null));
 
-  //     dispatch(setIntro(data));
-  //   })
-  //   .catch((err) => {
-  //     dispatch(setIntroLoading(false));
-  //     dispatch(setIntroError(err));
-  //   });
+      // dispatch(setIntro(data));
+      dispatch(setIntro([
+        { label: 'TOTAL POPULATION (2015)', percentage: null, value: '39,066,910' },
+        { label: 'RURAL POPULATION PERCENTAGE', percentage: '87.2%', value: '28,010,974' },
+        { label: 'TOTAL POPULATION WITHIN 5KM OF ALL ACESS POINTS', percentage: '71.7%', value: '28,010,974' },
+        { label: 'URBAN POPULATION PERCENTAGE:', percentage: '12.8%', value: '28,010,974' }
+      ]))
 
-  dispatch(setIntro([{ label: 'TOTAL POPULATION (2015)', percentage: null, value: '39,066,910' },
-                     { label: 'RURAL POPULATION PERCENTAGE', percentage: '87.2%', value: '28,010,974' },
-                     { label: 'TOTAL POPULATION WITHIN 5KM OF ALL ACESS POINTS', percentage: '71.7%', value: '28,010,974' },
-                     { label: 'URBAN POPULATION PERCENTAGE:', percentage: '12.8%', value: '28,010,974' }]))
+    })
+    .catch((err) => {
+      dispatch(setIntroLoading(false));
+      dispatch(setIntroError(err));
+    });
+
 });
 
 export default {
