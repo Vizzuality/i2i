1909864ec280fb39823a4b9ca13e1bd80d41a2f5
Ivan Birkman <ivan.yk14@gmail.com>
2018-07-26 16:23:26 +0200

    Add bbox field to country

--- app/models/country.rb
+++ app/models/country.rb
@@ -7,6 +7,7 @@
 #  updated_at :datetime         not null
 #  name       :string
 #  iso        :string
+#  bbox       :string           default([]), is an Array
 #
 
 class Country < ApplicationRecord
