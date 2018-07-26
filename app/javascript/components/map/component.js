4c0834628fbd914d378174311dd3930bc53bf74b
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-07-26 17:02:09 +0200

    Mask layer

--- app/javascript/components/map/component.js
+++ app/javascript/components/map/component.js
@@ -9,6 +9,31 @@ import { PluginLeaflet } from 'layer-manager';
 // styles
 import './styles.scss';
 
+const COUNTRY_MASK = {
+  id: 'country-mask',
+  name: 'Country Mask',
+  provider: 'carto',
+  layerConfig: {
+    account: 'ikerey',
+    body: {
+      maxzoom: 19,
+      minzoom: 2,
+      layers: [
+        {
+          type: 'cartodb',
+          options: {
+            sql: 'SELECT * FROM world_borders_hd',
+            cartocss: "#layer{polygon-fill:#2f939c;polygon-opacity:.9;line-color:#2f939c;line-opacity:.1;line-width:0}#layer[iso_a3='{{iso}}']{polygon-fill:#FFF;polygon-opacity:0;line-opacity:1}",
+            cartocss_version: '2.3.0'
+          }
+        }
+      ]
+    }
+  },
+  legendConfig: {},
+  interactionConfig: {}
+};
+
 class SidebarComponent extends React.Component {
   static propTypes = {
     open: PropTypes.bool.isRequired,
@@ -33,6 +58,13 @@ class SidebarComponent extends React.Component {
                   activeLayers.map((layer, index) =>
                     <Layer key={layer.id} {...layer} zIndex={1000 - index} />)
                 }
+
+                <Layer
+                  {...COUNTRY_MASK}
+                  key="country-mask"
+                  params={{ iso: this.props.iso }}
+                  zIndex={1001}
+                />
               </LayerManager>
             </React.Fragment>
             )}
