cb80ff8ec2375b6057b26bc4d00a24308d30fe33
Ivan Birkman <ivan.yk14@gmail.com>
2018-07-31 12:25:46 +0200

    Add remove layer through legend

--- app/javascript/components/map/legend/index.js
+++ app/javascript/components/map/legend/index.js
@@ -9,13 +9,19 @@ import { getActiveLayerGroups } from 'components/map/selectors';
 // Components
 import LegendComponent from './component';
 
+// Actions
+import { setSelectedLayers as setSelectedSectorLayers } from 'components/sidebar/sectors/actions';
+import { setSelectedLayers as setSelectedContextualLayers } from 'components/sidebar/contextual-layers/actions';
+
 // Mandatory
 export { actions, reducers, initialState };
 
 export default connect(
   state => ({
     ...state.legend,
-    activeLayerGroups: getActiveLayerGroups(state)
+    activeLayerGroups: getActiveLayerGroups(state),
+    selectedSectorLayers: state.sectorLayers.selectedLayers,
+    selectedContextualLayers: state.contextualLayers.selectedLayers
   }),
-  actions
+  { actions, setSelectedSectorLayers, setSelectedContextualLayers }
 )(LegendComponent);
