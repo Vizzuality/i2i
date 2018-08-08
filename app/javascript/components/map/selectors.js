a643ecbdc5bbdbc17946c2a24383767c72a92740
Ivan Birkman <ivan.yk14@gmail.com>
2018-08-08 09:56:09 +0200

    Refactor repeated code in map selector

--- app/javascript/components/map/selectors.js
+++ app/javascript/components/map/selectors.js
@@ -6,33 +6,41 @@ const selectedLayers = state => state.fspMaps.layers.selectedLayers;
 const layersSettings = state => state.fspMaps.legend.layersSettings;
 const layersOrder = state => state.fspMaps.layers.layersOrder;
 
+function getSortedLayers(allLayersOrder, mapped) {
+  let sortedLayers = [];
+  let mappedLayers = mapped;
+
+  if (allLayersOrder.length === 0) {
+    sortedLayers = mappedLayers;
+  } else {
+    allLayersOrder.forEach((key) => {
+      let found = false;
+      mappedLayers = mappedLayers.filter((item) => {
+        if (!found && item.id == key) {
+          sortedLayers.push(item);
+          found = true;
+          return false;
+        } return true;
+      });
+    });
+  }
+
+  return sortedLayers;
+}
+
 export const getActiveLayers = createSelector(
   [layersList, selectedLayers, layersSettings, layersOrder],
   (_layersList, _selectedLayers, _layersSettings, _layersOrder) => {
-    let sortedLayers = [];
     const activeLayers = _layersList.filter(layer => _selectedLayers.includes(layer.id));
     const allLayersOrder = _layersOrder.concat(difference(_selectedLayers, _layersOrder));
 
-    let mapped = activeLayers.map(l => ({
+    const mapped = activeLayers.map(l => ({
       ...l,
       visibility: _layersSettings[l.id] ? _layersSettings[l.id].visibility : true,
       opacity: (_layersSettings[l.id] && _layersSettings[l.id].opacity) ? _layersSettings[l.id].opacity : 1
     }));
 
-    if (allLayersOrder.length === 0) {
-      sortedLayers = mapped;
-    } else {
-      allLayersOrder.forEach((key) => {
-        let found = false;
-        mapped = mapped.filter((item) => {
-          if (!found && item.id == key) {
-            sortedLayers.push(item);
-            found = true;
-            return false;
-          } return true;
-        });
-      });
-    }
+    const sortedLayers = getSortedLayers(allLayersOrder, mapped);
 
     return sortedLayers;
   }
@@ -41,11 +49,10 @@ export const getActiveLayers = createSelector(
 export const getActiveLayerGroups = createSelector(
   [layersList, selectedLayers, layersSettings, layersOrder],
   (_layersList, _selectedLayers, _layersSettings, _layersOrder) => {
-    let sortedLayers = [];
     const activeLayers = _layersList.filter(layer => _selectedLayers.includes(layer.id));
     const allLayersOrder = _layersOrder.concat(difference(_selectedLayers, _layersOrder));
 
-    let mapped = activeLayers.map(l => ({
+    const mapped = activeLayers.map(l => ({
       dataset: l.id,
       id: l.id,
       visibility: _layersSettings[l.id] ? _layersSettings[l.id].visibility : true,
@@ -53,20 +60,7 @@ export const getActiveLayerGroups = createSelector(
       layers: [{ ...l, active: true }]
     }));
 
-    if (allLayersOrder.length === 0) {
-      sortedLayers = mapped;
-    } else {
-      allLayersOrder.forEach((key) => {
-        let found = false;
-        mapped = mapped.filter((item) => {
-          if (!found && item.id == key) {
-            sortedLayers.push(item);
-            found = true;
-            return false;
-          } return true;
-        });
-      });
-    }
+    const sortedLayers = getSortedLayers(allLayersOrder, mapped);
 
     return sortedLayers;
   }
