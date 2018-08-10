dd3d03745fb678a598922ed942839e5ae52ba78e
Ivan Birkman <ivan.yk14@gmail.com>
2018-08-10 10:36:39 +0200

    Remove comments

--- app/javascript/components/map/popup/component.js
+++ app/javascript/components/map/popup/component.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 
 import { MapPopup } from 'wri-api-components';
-
-// styles
-// import './styles.scss';
 
 class PopupComponent extends React.Component {
   static propTypes = {
     map: PropTypes.object.isRequired,
     interaction: PropTypes.object
-    // data: PropTypes.object.isRequired,
-    // onClickInfo: PropTypes.func
   }
 
-  static defaultProps = {
-    interaction: null
-    // onClickInfo: null
-  }
+  static defaultProps = { interaction: null }
 
   render() {
     const { map, interaction } = this.props;
