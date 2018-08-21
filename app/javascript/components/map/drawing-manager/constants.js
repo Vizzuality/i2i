60934cd2fc58476221d596d22b2042737128cdfc
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-08-21 12:53:40 +0200

    Polygons: markers and polygons styles

--- app/javascript/components/map/drawing-manager/constants.js
+++ app/javascript/components/map/drawing-manager/constants.js
@@ -1,3 +1,30 @@
-export const DRAWING_OPTIONS = { cursorMarker: true };
+export const DRAWING_STYLES = {
+  color: 'red',
+  fillColor: 'orange',
+  fillOpacity: 0.7,
+  strokeLinecap: 'round',
+  strokeLinejoin: 'round'
+}
+
+export const DRAWING_OPTIONS = {
+  snappable: true,
+  // Styles
+  templineStyle: {
+    ...DRAWING_STYLES
+  },
+  hintlineStyle: {
+    ...DRAWING_STYLES,
+    dashArray: [5, 5],
+  },
+  pathOptions: DRAWING_STYLES,
+  
+  cursorMarker: true,
+  markerStyle: {
+    opacity: 0.5,
+    draggable: false
+  },
+
+  finishOnDoubleClick: true
+};
 
 export default { DRAWING_OPTIONS };
