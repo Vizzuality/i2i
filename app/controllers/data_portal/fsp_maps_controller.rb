8f5df064fa47f4a6d757cb9b112693e89e05f5d6
Ivan Birkman <ivan.yk14@gmail.com>
2018-08-27 17:29:17 +0200

    Fix country selector selected value

--- app/controllers/data_portal/fsp_maps_controller.rb
+++ app/controllers/data_portal/fsp_maps_controller.rb
@@ -2,7 +2,7 @@ class DataPortal::FspMapsController < ApplicationController
   def show
     @iso = params[:iso]
     @country = Country.find_by(iso: @iso)
-    @countries = Country.all_except(@country)
+    @countries = Country.where(has_fsp_maps: true)
     render layout: 'data_portal'
   end
 end
