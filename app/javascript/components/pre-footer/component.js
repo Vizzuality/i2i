6cfe313e9f69f5c77ce4fc4048b13ab8b5957941
Ivan Birkman <ivan.yk14@gmail.com>
2018-09-05 17:41:11 +0200

    Add the add your own dataset text to fsp country

--- app/javascript/components/pre-footer/component.js
+++ app/javascript/components/pre-footer/component.js
@@ -16,10 +16,13 @@ class PreFooter extends PureComponent {
                 <div className="grid-s-12 grid-l-8">
                   <h3>Add your own data sets.</h3>
 
-                  <p>Lorem ipsum ullam id dolor id nibh ultricies vehicula
-                    ut id elit. Donec id elit non mi porta gravida at eget metus.
-                    Donec id elit non mi porta gravida at eget metus. Integer posuere
-                    erat a ante venenatis dapibus posuere velit aliquet.
+                  <p>
+                    If you have your own geospatial data that you would like to
+                    deposit on the i2i platform to enhance financial inclusion
+                    in Africa and Asia, please email us on <a href="mailto:info@i2ifacility.org">info@i2ifacility.org</a>.
+                    We will then contact you with the next steps. In the near
+                    future, you will be able to log in and upload your data
+                    directly onto our platform.
                   </p>
                 </div>
               </div>
