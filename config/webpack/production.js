54631076bf5d7e8aa89947b493d7e4cea67e4e20
Ivan Birkman <ivan.yk14@gmail.com>
2018-09-19 12:48:57 +0200

    Lower ecma config for webpacker prod

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+environment.plugins.get("UglifyJs").options.uglifyOptions.ecma = 5
+
 module.exports = environment.toWebpackConfig()
 
 // // webpack.config.js
