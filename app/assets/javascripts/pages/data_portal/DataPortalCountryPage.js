44cf2e8da82e39a46cda3da3f0926de6b4e40f06
Tiago Santos <santos.tiago@gmail.com>
2018-12-12 17:59:52 +0100

    Changed the data portal sources for nepal, laos and cambodia

--- app/assets/javascripts/pages/data_portal/DataPortalCountryPage.js
+++ app/assets/javascripts/pages/data_portal/DataPortalCountryPage.js
@@ -368,7 +368,10 @@
         country: App.Helper.Indicators.COUNTRIES[this.options.iso],
         population: this._getReadablePopulation(),
         isUNCDF: {
-          'Myanmar': 2018
+          'Myanmar': 2018,
+          'Cambodia': 2015,
+          'Laos': 2014,
+          'Nepal': 2014
         }[App.Helper.Indicators.COUNTRIES[this.options.iso]] === this.options.year,
         isAFR: App.Helper.Indicators.COUNTRIES[this.options.iso] === 'Rwanda' &&
           [2008, 2012, 2016].includes(this.options.year),
