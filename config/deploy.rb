dd0aae2db1df5925bc1346a42ddda1966d179502
Tiago Garcia <tiagojsag@gmail.com>
2019-03-07 11:31:41 +0100

    Config Capistrano for staging deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,17 @@
 lock "3.7.1"
 
 set :application, 'i2i'
-set :repo_url, 'git@github.com:Vizzuality/i2i.git'
+set :repo_url, 'https://github.com/Vizzuality/i2i.git'
 set :deploy_to, '/var/www/i2i'
 set :use_sudo, true
+set :rvm_custom_path, '/usr/share/rvm'
 
 set :deploy_user, 'ubuntu'
-set :rvm_ruby_version, '2.3.1'
-set :keep_releases, 5
+set :rvm_ruby_version, '2.5.3'
+set :keep_releases, 3
 set :use_sudo, true
-set :linked_files, %w{.env}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{.env public/robots.txt}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/ckeditor_assets db/data}
 
 namespace :deploy do
   after :finishing, 'deploy:cleanup'
