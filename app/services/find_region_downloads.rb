45b3d25d9204464bf1ebdf6299a8ea47ee7931e0
David Inga <david.inga@vizzuality.com>
2019-06-03 16:53:02 +0200

    added label National surveys to region data

--- app/services/find_region_downloads.rb
+++ app/services/find_region_downloads.rb
@@ -1,20 +1,20 @@
 class FindRegionDownloads
   attr_reader :region
-  
+
   delegate :iso, to: :region
-  
+
   def initialize(region)
     @region = region
-    
+
     region.links.first(4)
   end
-  
+
   def perform
     region_links_downloads | region_api_downloads
   end
-  
+
   private
-  
+
   def region_links_downloads
     region.links.map do |link|
       OpenStruct.new(
@@ -26,10 +26,10 @@ class FindRegionDownloads
       )
     end
   end
-  
+
   def region_api_downloads
     result = []
-    
+
     if region_all_years.dig('years').present?
       region_all_years['years'].map do |year_data|
         if year_data['dataUrl'].present?
@@ -40,24 +40,24 @@ class FindRegionDownloads
             zip: true
           )
         end
-    
+
         result << OpenStruct.new(
-          name: year_data['year'].to_s,
+          name: "National Surveys data (#{year_data['year'].to_s})",
           url: "#{ENV['API_URL']}/region/#{region.iso}/#{year_data['year']}/download",
           html_class: "api-#{year_data['year']}",
           zip: false
         )
       end
     end
-    
+
     result
   end
 
   def region_all_years
     response = JSON.parse(HTTP.get("#{ENV['API_URL']}/region/#{region.iso}").body.to_s)
-    
+
     return {} if response.is_a?(Hash) && response['errors'].present?
-    
+
     response.first
   end
 end
