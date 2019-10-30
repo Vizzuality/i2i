8703930b1e2449eefc336ec59bb3bd083ccb5b81
María Luena <maria.luena@vizzuality.com>
2019-10-30 14:31:24 +0100

    icon info in legend

--- app/javascript/components/map/legend/legend-item-button-info/component.js
+++ app/javascript/components/map/legend/legend-item-button-info/component.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Icon from 'wri-api-components/dist/icon';
+import Icon from 'components/icon';
 import Tooltip from 'wri-api-components/dist/tooltip';
 
 // components
@@ -10,43 +10,42 @@ import LegendButtonInfoTooltip from './legend-item-button-info-tooltip';
 // styles
 import './styles.scss';
 
-const LegendButtonInfo = ({ info }) => {
-  return (
-    <div className="c-legend-item-button-info">
+const LegendButtonInfo = ({ info }) => (
+
+  <div className="c-legend-item-button-info">
+    <Tooltip
+      overlay="Layer info"
+      overlayClassName="c-rc-tooltip -default"
+      overlayStyle={{ color: '#fff' }}
+      placement="top"
+      trigger={['hover']}
+      mouseLeaveDelay={0}
+      destroyTooltipOnHide
+    >
       <Tooltip
-        overlay="Layer info"
+        overlay={
+          <LegendButtonInfoTooltip
+            title="Info"
+            info={info}
+          />
+        }
         overlayClassName="c-rc-tooltip -default"
         overlayStyle={{ color: '#fff' }}
         placement="top"
-        trigger={['hover']}
+        trigger={['click']}
         mouseLeaveDelay={0}
         destroyTooltipOnHide
       >
-        <Tooltip
-          overlay={
-            <LegendButtonInfoTooltip
-              title="Info"
-              info={info}
-            />
-          }
-          overlayClassName="c-rc-tooltip -default"
-          overlayStyle={{ color: '#fff' }}
-          placement="top"
-          trigger={['click']}
-          mouseLeaveDelay={0}
-          destroyTooltipOnHide
+        <button
+          type="button"
+          className="wri_api__c-legend-button legend-info"
         >
-          <button
-            type="button"
-            className="wri_api__c-legend-button"
-          >
-            <Icon name="icon-info" className="-small" />
-          </button>
-        </Tooltip>
+          <Icon name="info" className="-small" />
+        </button>
       </Tooltip>
-    </div>
-  );
-};
+    </Tooltip>
+  </div>
+);
 
 LegendButtonInfo.propTypes = { info: PropTypes.shape({}).isRequired };
 
