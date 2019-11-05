649fd08e4dd346d1c8a5512035a3bba3a430744c
María Luena <maria.luena@vizzuality.com>
2019-11-05 17:04:37 +0100

    WIP: widget templates first approach

--- app/javascript/components/widget/widget-templates/number-of-services/component.js
+++ app/javascript/components/widget/widget-templates/number-of-services/component.js
@@ -7,36 +7,17 @@ import Chart from 'components/recharts';
 import config from './config';
 
 const NumberOfServices = ({ widgetData, widget, title }) => {
-console.log(widgetData)
-console.log('number os services')
-  // if (noData) {
-  //   return null
-  //   //  <NoData />
 
-
-  // }
   return (
     <Fragment>
       <div className="c-widget-template">
         <h1>{title}</h1>
       </div>
-
-
-
-        {/* <Chart
-          data={chart}
-          config={config}
-        /> */}
+        <Chart />
     </Fragment>
   );
 };
 
 
-// NumberOfServices.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   chart: PropTypes.array.isRequired,
-//   config: PropTypes.shape({}).isRequired,
-//   noData: PropTypes.bool.isRequired
-// };
 
 export default NumberOfServices;
