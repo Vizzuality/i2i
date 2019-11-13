682d759d47d859f771ce9bf67a0dc9bfa679a66c
María Luena <maria.luena@vizzuality.com>
2019-11-13 16:31:14 +0100

    WIP: N of services widget

--- app/javascript/components/widget/legend/component.js
+++ app/javascript/components/widget/legend/component.js
@@ -11,21 +11,18 @@ const Legend = ({ data }) => (
         <li
           key={d.dateKey}
         >
-                       <svg height="12" width="12">
-                <rect
-                  className="legend-color"
-                  fill={d.color}
-                />
-              </svg>
+          <svg height="12" width="12">
+            <rect
+              className="legend-color"
+              fill={d.color}
+            />
+          </svg>
 
           {d.dataKey}
         </li>
       ))}
 
     </ul>
-
-
-{console.log(data, 'legend')}
   </div>
 );
 
@@ -36,30 +33,3 @@ Legend.defaultProps = {
 };
 
 export default Legend;
-
-
-// <ul className={classnames(styles.widget_legend_list, styles[`_${type}`])}>
-//           {groups[g].map((item, i) => (
-//             <li
-//               key={`item-${i + 1}-${item.color}`}
-//               className={classnames(styles.widget_legend_list_item,
-//                 styles[`_${item.variant || variant}`],
-//                 styles[`_${type}`])
-//               }
-//             >
-//               <svg height="12" width="12">
-//                 <rect
-//                   className={classnames(styles.item, styles[`_${type}`])}
-//                   fill={item.color}
-//                 />
-//               </svg>
-
-//               <div className={styles.itemWrapper}>
-//                 <span>{item.value}</span>
-//                 {item.payload && item.payload.y
-//                   && <span className={styles.item}>{`${numberFormat(item.payload.y)} ${unit}`}</span>
-//                 }
-//               </div>
-//             </li>
-//           ))}
-//         </ul>
