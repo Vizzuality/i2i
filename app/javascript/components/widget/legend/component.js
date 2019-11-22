067b7674f28f3afaa8656a859715a4fddea0ec9e
María Luena <maria.luena@vizzuality.com>
2019-11-22 13:14:07 +0100

    view more btn

--- app/javascript/components/widget/legend/component.js
+++ app/javascript/components/widget/legend/component.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import './styles.scss';
 
-const Legend = ({ data }) => (
-  <div className="c-widget-legend">
-    <ul className="legend-list">
-      {data.map(d => (
-        <li
-          key={d.dataKey || d.label || d.value}
-        >
-          <span style={{
-            color: d.color,
-            backgroundColor: d.color
-          }}
-          />
-          {d.label || d.dataKey || d.value}
-        </li>
-      ))}
-    </ul>
-    <button className="legend-btn">View more</button>
-  </div>
-);
+const Legend = ({ data }) => {
+  const [menu, setMenu] = useState(false);
+  const { length } = data;
+
+
+  return (
+    <div className="c-widget-legend">
+      {!menu &&
+        <p className="widget-ranking">{length >= 5 ? `TOP ${length}` : 'TOP 5'}</p>
+      }
+      <ul className={classnames('legend-list', { isOpen: menu })}>
+        {data.map(d => (
+          <li
+            key={d.dataKey || d.label || d.value}
+          >
+            <span style={{
+              color: d.color,
+              backgroundColor: d.color
+            }}
+            />
+            {d.label || d.dataKey || d.value}
+          </li>
+        ))}
+      </ul>
+      {length > 5 &&
+        <button onClick={() => setMenu(!menu)} className="legend-btn">View more</button>
+      }
+    </div>
+  );
+};
 
 Legend.propTypes = { data: PropTypes.shape({}) };
 
