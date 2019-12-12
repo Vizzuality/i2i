26e855166610d511c4775d7b8e235b768c3f2283
“mluena” <maluenarod@gmail.com>
2019-12-12 12:52:40 +0100

    open search in header

--- app/assets/javascripts/pages/static_pages/search.js
+++ app/assets/javascripts/pages/static_pages/search.js
@@ -1,45 +1,33 @@
 (function (App) {
-    App.Page.SearchPage = Backbone.View.extend({
+  App.View.Search = Backbone.View.extend({
+    initialize: function () {
+      this._setVars();
+      this._setEventListeners();
+    },
 
-        el: 'body',
+    _setVars: function() {
+      this.$searchContainer = $('#search-container');
+      this.$searchButton = $('#search-button');
+      this.$closeButton = $('#close-button');
+      this.$searchInput = $('#search-input');
+    },
 
-        events: {
-            'click .js-search-filters': '_openFiltersModal',
-            'submit #search-input': '_handleSearchInput'
-        },
+    _setEventListeners: function () {
+      this.$searchInput = $('#search-input');
+      this.$searchButton.on('click', this._toggleSearch.bind(this));
+      this.$closeButton.on('click', this._closeSearch.bind(this));
+    },
 
-        initialize: function (term, tagTerm) {
-            this.term = gon.term || [];
-            this.tagTerm = gon.selected_tags || [];
-            this._setEventListeners();
-        },
+    _toggleSearch: function () {
+      this.$searchContainer.toggleClass('-open');
+      if (this.$searchContainer.hasClass('-open')) {
+        this.$searchInput.focus()
+      }
+    },
 
-        _setEventListeners: function () {
-            Backbone.Events.on('search:update', this._handleSearchTags.bind(this));
-          },
+    _closeSearch: function () {
+      this.$searchContainer.removeClass('-open');
+    }
+  });
 
-        _handleSearchTags: function(event) {
-            this.tagTerm = event['tag_term'];
-            this._updateSearch();
-        },
-
-        _handleSearchInput: function(event) {
-            event.preventDefault();
-            this.term = $(event.currentTarget).find('.search-input').val();
-            this._updateSearch();
-        },
-
-        _updateSearch: function() {
-            var terms = $.param({
-                term: this.term,
-                tag_term: this.tagTerm.length > 0 ? this.tagTerm.join(',') : []
-            });
-
-            window.location.href = '/search?' + terms;
-        },
-
-        _openFiltersModal: function() {
-            new App.View.SearchFiltersModal({selectedTags: this.tagTerm});
-        }
-    });
 }).call(this, this.App);
