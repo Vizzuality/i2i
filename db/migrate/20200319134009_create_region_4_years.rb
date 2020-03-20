9b543a578c9fdfa852fd25a2f6b6adc059d5cbde
jesusgonzalezrivera <jesus198927@gmail.com>
2020-03-20 15:12:18 +0100

    Fix migration for region_4_years table

--- db/migrate/20200319134009_create_region_4_years.rb
+++ db/migrate/20200319134009_create_region_4_years.rb
@@ -1,6 +1,13 @@
 class CreateRegion4Years < ActiveRecord::Migration[5.2]
   def change
-    create_table :region4_years do |t|
+    create_table :region_4_years do |t|
+      t.integer :year
+      t.float :total
+      t.string :data_url
+
+      t.references :region
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,13 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
+  create_table "active_admin_comments", id: :serial, force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_id", null: false
     t.string "resource_type", null: false
     t.string "author_type"
-    t.bigint "author_id"
+    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.text "content"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -63,8 +63,8 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.string "slug"
     t.boolean "published"
     t.string "custom_author"
-    t.string "record_type", default: "blog"
     t.integer "category_id"
+    t.string "record_type", default: "blog"
     t.boolean "is_featured", default: false
     t.integer "position"
   end
@@ -244,7 +244,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.string "name"
     t.string "file_file_name"
     t.string "file_content_type"
-    t.bigint "file_file_size"
+    t.integer "file_file_size"
     t.datetime "file_updated_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -256,7 +256,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.text "content"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -266,8 +266,8 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.string "slug"
     t.boolean "published"
     t.string "custom_author"
-    t.string "record_type", default: "event"
     t.integer "category_id"
+    t.string "record_type", default: "event"
     t.boolean "is_featured", default: false
     t.integer "position"
   end
@@ -506,7 +506,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.text "summary"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.datetime "date"
     t.string "url_resource"
@@ -514,8 +514,8 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.string "issuu_link"
     t.string "slug"
     t.boolean "published"
-    t.string "record_type", default: "library"
     t.integer "category_id"
+    t.string "record_type", default: "library"
     t.boolean "is_featured", default: false
     t.integer "position"
     t.text "description"
@@ -569,7 +569,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.string "role"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.string "slug"
     t.datetime "created_at", null: false
@@ -586,15 +586,15 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.text "content"
     t.string "image_file_name"
     t.string "image_content_type"
-    t.bigint "image_file_size"
+    t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.datetime "date"
     t.string "author"
     t.string "issuu_link"
     t.string "slug"
     t.boolean "published"
-    t.string "record_type", default: "news"
     t.integer "category_id"
+    t.string "record_type", default: "news"
     t.boolean "is_featured", default: false
     t.integer "position"
   end
@@ -647,7 +647,14 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.text "custom_text"
   end
 
-  create_table "region4_years", force: :cascade do |t|
+  create_table "region_4_years", force: :cascade do |t|
+    t.integer "year"
+    t.float "total"
+    t.string "data_url"
+    t.bigint "region_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["region_id"], name: "index_region_4_years_on_region_id"
   end
 
   create_table "region_partners", id: :serial, force: :cascade do |t|
@@ -671,11 +678,12 @@ ActiveRecord::Schema.define(version: 2020_03_19_134009) do
     t.index ["slug"], name: "index_regions_on_slug", unique: true
   end
 
-  create_table "sessions", id: :serial, force: :cascade do |t|
+  create_table "sessions", id: false, force: :cascade do |t|
+    t.serial "id", null: false
     t.string "session_id", null: false
     t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
