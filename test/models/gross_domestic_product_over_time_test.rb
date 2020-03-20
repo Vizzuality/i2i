49ebcc8f0cae66760ad0ee06c3d0623759554cce
jesusgonzalezrivera <jesus198927@gmail.com>
2020-03-20 13:57:28 +0100

    Remove unit tests

--- test/models/gross_domestic_product_over_time_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GrossDomesticProductOverTimeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/population_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PopulationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
