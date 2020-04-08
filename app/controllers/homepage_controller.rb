765772933da6baa20b217ef7dc9baa8993e8047e
jesusgonzalezrivera <jesus198927@gmail.com>
2020-04-08 10:33:39 +0200

    Improve performance of data_portal page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -10,7 +10,13 @@ class HomepageController < ApplicationController
 
     @tweets = TwitterApi.get_tweets
     @insights = records.flatten.sort do |a, b|
-      [[a.featured_position ? 0 : 1, a.featured_position ? a.featured_position.position : nil], b[:updated_at]] <=> [[b.featured_position ? 0 : 1, b.featured_position ? b.featured_position.position : nil], a[:updated_at]]
+      [
+        [a.featured_position ? 0 : 1, a.featured_position ? a.featured_position.position : nil],
+        b[:updated_at]
+      ] <=> [
+        [b.featured_position ? 0 : 1, b.featured_position ? b.featured_position.position : nil],
+        a[:updated_at]
+      ]
     end[0..5]
   end
 end
